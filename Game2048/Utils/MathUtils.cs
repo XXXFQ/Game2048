c8c79e18d6acbd4f0820b58c5693f24e6e93e807
ARM <leiomama117@gmail.com>
2023-03-13 03:04:57 +0900

    数値に関するユーティリティクラスの追加 `v1.2.0`

---
+++ Game2048/Utils/MathUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game2048.Utils
+{
+    internal static class MathUtils
+    {
+        /// <summary>
+        /// 対数(log10)を取って数値の桁数を調べる
+        /// </summary>
+        public static int GetDigitCount(int value)
+        {
+            // NegativeInfinityを回避
+            return (value == 0) ? 1 : ((int)Math.Log10(value) + 1);
+        }
+    }
+}
